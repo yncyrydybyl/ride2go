462ea739e17a3311532dd0e785c8cc132e48a037
Stefan Plantikow <stefan.plantikow@googlemail.com>
2012-10-11 20:09:05 +0200

    Integrates all tests back into active_tests

--- test/active_tests.js
+++ test/active_tests.js
@@ -1,16 +1,43 @@
 // configure tests
 var selected_tests = {
-    base: true
+    base: true,
+    place: true,
+    ride: true,
+    import: true,
+    connectors: true
 };
 
 // result
 var active_tests = [];
 
-if (selected_tests.base) {
-    active_tests.push("test/base/objset_test.coffee");
+active_tests.push("test/mocha_test.coffee");
+
+if (selected_tests.connectors) {
+//    active_tests.push("test/connectors/pts_test.coffee");
+//    active_tests.push("test/connectors/citynetz_test.coffee");
+//    active_tests.push("test/connectors/deinbus_test.coffee");
+//    active_tests.push("test/connectors/geoname_test.coffee");
+//    active_tests.push("test/connectors/mapquest_test.coffee");
+//    active_tests.push("test/connectors/mitfahrzentrale_test.coffee");
+//    active_tests.push("test/connectors/raummobil_test.coffee");
 }
 
-active_tests.push("test/base/leafy_test.coffee");
+if (selected_tests.base) {
+    active_tests.push("test/base/objset_test.coffee");
+    active_tests.push("test/base/leafy_test.coffee");
+}
+
+if (selected_tests.place) {
+    active_tests.push("test/place_test.coffee");
+}
+
+if (selected_tests.ride) {
+    active_tests.push("test/ride_test.coffee");
+}
+
+if (selected_tests.import) {
+    active_tests.push("test/geoname_import_test.coffee");
+}
 
 // export
 module.exports = active_tests;
